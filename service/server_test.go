86a10df5cd4238818d0e11d7c88a5070e3ff0871
Kaustubh Mallik <kaustubhmallik@gmail.com>
2019-12-27 08:32:46 +0100

    Add delay in the server test

--- service/server_test.go
+++ service/server_test.go
@@ -4,15 +4,17 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestStartServer(t *testing.T) {
 	go func() {
-		_ = StartServer("localhost", "34517")
+		_ = StartServer("localhost", "39597")
 	}()
+	time.Sleep(2 * time.Second)
 
 	//connect to server
-	conn, err := net.Dial("tcp", "localhost:34517")
+	conn, err := net.Dial("tcp", "localhost:39597")
 	assert.NoError(t, err, "connection unsuccessful")
 	assert.NotEmpty(t, conn, "connection unsuccessful")
 
