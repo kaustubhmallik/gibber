142b528b258b2e45c0b574a7dbe612de344f1fe4
Kaustubh Mallik <kaustubhmallik@gmail.com>
2019-12-29 23:35:03 +0100

    Revert "Comment out server_test (need to fix it)"
    This reverts commit b812507b60df9fcc7d61dea63d1e876244c4e5ac.

--- service/server_test.go
+++ service/server_test.go
@@ -1,18 +1,20 @@
 package service
 
 import (
+	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
 )
 
 func TestStartServer(t *testing.T) {
-	//go func() {
-	//	_ = StartServer("localhost", "34510")
-	//}()
-	//
+	go func() {
+		_ = StartServer("localhost", "34510")
+	}()
+
 	//connect to server
-	//conn, err := net.Dial("tcp", "localhost:34510")
-	//assert.NoError(t, err, "connection unsuccessful")
-	//assert.NotEmpty(t, conn, "connection unsuccessful")
-	//
-	//establishClientConnection(&conn)
+	conn, err := net.Dial("tcp", "localhost:34510")
+	assert.NoError(t, err, "connection unsuccessful")
+	assert.NotEmpty(t, conn, "connection unsuccessful")
+
+	establishClientConnection(&conn)
 }
