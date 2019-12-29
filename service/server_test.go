4165ecfa03ff37ed5106d18f01bb41b0d674b5a2
Kaustubh Mallik <kaustubhmallik@gmail.com>
2019-12-29 23:36:22 +0100

    Add sleep b/w server start and client connect

--- service/server_test.go
+++ service/server_test.go
@@ -4,12 +4,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestStartServer(t *testing.T) {
 	go func() {
 		_ = StartServer("localhost", "34510")
 	}()
+	time.Sleep(time.Second * 3)
 
 	//connect to server
 	conn, err := net.Dial("tcp", "localhost:34510")
